4a2c41634009b8602db025b3ffc7239c25f79aa4
gd744 <gonzalezdavid7761@gmail.com>
2021-04-29 20:07:11 -0700

    Finish Structured English

---
+++ Test2.py
@@ -0,0 +1,6 @@
+"""
+This program will be used to calculate and display the average of 24 final grades in a class.
+It will also display and calculate the percentage of grades above the average grade.
+
+The 24 grades will be read from the file "Final.txt" using the infile open command
+"""
