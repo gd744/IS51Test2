df6b69875e4e558e0de4487d0211384a5dbce2b5
gd744 <gonzalezdavid7761@gmail.com>
2021-04-29 20:13:11 -0700

    Finish psuedocode

--- Test2.py
+++ Test2.py
@@ -4,3 +4,22 @@ It will also display and calculate the percentage of grades above the average gr
 
 The 24 grades will be read from the file "Final.txt" using the infile open command
 """
+
+"""
+PSUEDOCODE 
+
+main
+    infile = open final.txt
+    copy each grade individually with linerstrip
+close the infile
+print number of grades in file to confirm
+
+calculate the percent above average()
+
+    class_average = sum of all grades / 24(total #)
+    For loop of grade in grades 
+        if grade > class_average:
+            num +=1
+    print("Class Average: , class_average)
+    print("% of grades above the class average: , )
+"""
